eeff254d1184d79250ad9fd5cc2dc5b86b3c18ab
marco-rozo <marcorozo99@gmail.com>
2023-05-22 13:44:52 -0300

    chore: create dto

---
+++ src/types/userInterfaceDTO.ts
@@ -0,0 +1,20 @@
+export interface IUserResponse {
+  email: string;
+  name: string;
+}
+
+export interface IUser extends IUserResponse {
+  id?: number;
+  password?: string;
+}
+
+export function fromUserResponse(user: IUser): IUserResponse {
+  
+  let userResponse: IUserResponse ={
+    email: user.email,
+    name: user.name
+  }
+  
+  return userResponse;
+}
+
