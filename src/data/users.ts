62a44c12201c3dafd85891883e5c960e80fa700a
marco-rozo <marcorozo99@gmail.com>
2023-05-22 13:44:39 -0300

    chore: create users fake list

---
+++ src/data/users.ts
@@ -0,0 +1,22 @@
+import { IUser } from '../types/userInterfaceDTO';
+
+export const users: IUser[] = [
+  {
+    id: 1,
+    email: "marco123@gmail.com",
+    name: "Marco Antonio Rozo",
+    password: "marco123",
+  },
+  {
+    id: 2,
+    email: "teste123@gmail.com",
+    name: "Teste",
+    password: "teste123",
+  },
+  {
+    id: 3,
+    email: "flutter123@gmail.com",
+    name: "Flutter ",
+    password: "flutter123",
+  },
+];
