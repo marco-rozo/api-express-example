8957658d853a0e70e7aa6c6a05ae31f4e4335aa2
marco-rozo <marcorozo99@gmail.com>
2023-05-22 13:45:04 -0300

    feat: changed auth method

--- src/controllers/AuthController.ts
+++ src/controllers/AuthController.ts
@@ -1,45 +1,43 @@
 import { Request, Response } from "express";
 import bcrypt from 'bcryptjs';
 import jwt from 'jsonwebtoken';
+import { users } from "../data/users";
+import { IUser, IUserResponse, fromUserResponse } from "../types/userInterfaceDTO";
 
 class AuthController {
     async authenticate(req: Request, res: Response) {
         const { email, password } = req.body;
-        console.log(req.body)
 
         // Simulando "Busca" usuário
-        const user: any = {
-            id: 1,
-            email: "marco123@gmail.com",
-            name: "Marco Antonio Rozo",
-            password: "marco123"
-        };
+        const userExist: IUser | undefined = users.find(e=> e.email == email);
 
-        if (!user) {
+        if (!userExist) {
             return res.sendStatus(401);
         }
 
         // Simulando o password criptografado que estaria salvo no banco
-        user.password = bcrypt.hashSync(user?.password);
+        userExist.password = bcrypt.hashSync(userExist?.password!);
 
-        const isValidPassword = await bcrypt.compare(password, user.password);
-        const isValidEmail = user.email == email;
-
+        const isValidPassword = await bcrypt.compare(password, userExist.password);
+        const isValidEmail = userExist.email == email;
 
         if (!isValidPassword || !isValidEmail) {
             return res.status(401).json();
         }
 
         // "Configurando" token jwt
-        const token = jwt.sign({ id: user.id }, 'secret', { expiresIn: '1d' });
+        const token = jwt.sign({ id: userExist.id }, 'secret', { expiresIn: '1d' });
 
-        delete user.password;
+        // Utilizando o DTO para mapear os dados do usuário 
+        // para retornar apenas o necessário (removendo id e password do objeto)
+        const user: IUserResponse = fromUserResponse(userExist);
 
         return res.json({
             user,
             token
         });
     }
+    
 }
 
 export default new AuthController();
